1b9af9e7d5b53930a9d22831f438dcfbc4769bd5
Erik Hofer <erik-hofer@mail.de>
2018-11-11 19:52:26 +0100

    #3 add linting to client

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,24 +1,21 @@
-import { Rate } from 'antd';
-import * as React from 'react';
-import './App.scss';
-
-import logo from './logo.svg';
+import { Rate } from 'antd'
+import * as React from 'react'
+import './App.scss'
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <Rate character="6"/>
+        <Rate character="6" />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
