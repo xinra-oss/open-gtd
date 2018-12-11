82550b27a1a782ef10cc078dc7565e72d5949540
Tim Bestmann <tbestmann@web.de>
2018-12-12 00:24:43 +0100

    #15 add mongodb connection for creating user accounts

--- packages/server/src/router/user.ts
+++ packages/server/src/router/user.ts
@@ -1,13 +1,26 @@
 import { UserApi } from '@open-gtd/api'
 import { User } from '@open-gtd/model'
+import { MongoClient } from 'mongodb'
 import { RouterDefinition } from 'rest-ts-express'
 
 export const UserRouter: RouterDefinition<typeof UserApi> = {
   createUser: async req => {
     const user = req.body as User
-    // TODO: check email address for uniqueness before inserting to database
-    // TODO: return user object from database (id is created on inserting)
     // TODO: hash password before storing in database
-    return user
+
+    const db = await MongoClient.connect('mongodb://localhost:27017/')
+    const dbo = await db.db('open-gtd')
+    if (
+      (await dbo
+        .collection('users')
+        .find({ email: user.email })
+        .count()) > 0
+    ) {
+      // TODO: implement proper error handling
+      return 'mail address is already in use'
+    } else {
+      const insertedElement = await dbo.collection('users').insertOne(user)
+      return insertedElement.ops[0]
+    }
   }
 }
