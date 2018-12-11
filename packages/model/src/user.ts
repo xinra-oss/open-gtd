f7ee653850df517bebaf16fc3959a14724d19c43
Tim Bestmann <tbestmann@web.de>
2018-12-11 22:20:49 +0100

    add basic functions for user api

--- packages/model/src/user.ts
+++ packages/model/src/user.ts
@@ -1,10 +1,15 @@
 import { Record, Static, String } from 'runtypes'
+import { isEmail } from 'validator'
 import { Entity } from './entity'
 
 export const User = Entity.And(
   Record({
-    email: String,
-    password: String
+    email: String.withConstraint(
+      s => isEmail(s) || `email address is not valid`
+    ),
+    password: String.withConstraint(
+      s => s.length >= 8 || `password must be at least 8 characters long`
+    )
   })
 )
 
--- packages/server/src/router/user.ts
+++ packages/server/src/router/user.ts
@@ -5,7 +5,9 @@ import { RouterDefinition } from 'rest-ts-express'
 export const UserRouter: RouterDefinition<typeof UserApi> = {
   createUser: async req => {
     const user = req.body as User
-    // TODO
+    // TODO: check email address for uniqueness before inserting to database
+    // TODO: return user object from database (id is created on inserting)
+    // TODO: hash password before storing in database
     return user
   }
 }
