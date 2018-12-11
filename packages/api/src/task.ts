ed70ba37e28353f8c37d9afabaeaf39624684b5a
Tim Bestmann <tbestmann@web.de>
2018-12-12 00:40:53 +0100

    #26 add basic task api requirements

--- packages/api/src/task.ts
+++ packages/api/src/task.ts
@@ -1,9 +1,9 @@
-import { Task } from '@open-gtd/model'
+import { NewTask, Task } from '@open-gtd/model'
 import { defineAPI, DELETE, POST, PUT } from 'rest-ts-core'
 
 export const TaskApi = defineAPI({
   createTask: POST `/tasks` // prettier-ignore
-    .body(Task)
+    .body(NewTask)
     .response(Task),
   deleteTask: DELETE `/tasks/${'id'}`, // prettier-ignore
   updateTask: PUT `/tasks/${'id'}` // prettier-ignore
--- packages/server/src/router/task.ts
+++ packages/server/src/router/task.ts
@@ -1,10 +1,16 @@
 import { TaskApi } from '@open-gtd/api'
+import { Task } from 'packages/model/lib'
 import { RouterDefinition } from 'rest-ts-express'
 
 export const TaskRouter: RouterDefinition<typeof TaskApi> = {
   createTask: async req => {
-    const task = req.body
-    // TODO
+    const newTask = req.body
+    const task: Task = {
+      ...newTask,
+      userId: 'TODO'
+    }
+    // TODO: check if parent exists, if is stated (undefined or existing)
+    // TODO: check if user exists
     return task
   },
   deleteTask: async req => {
