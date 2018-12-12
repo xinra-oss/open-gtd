384656a842bbaa6d3176ea3c641b949ca028b6b5
Tim Bestmann <tbestmann@web.de>
2018-12-12 09:32:22 +0100

    #26 implement function for creating tasks

--- packages/server/src/router/task.ts
+++ packages/server/src/router/task.ts
@@ -1,17 +1,42 @@
 import { TaskApi } from '@open-gtd/api'
+import { MongoClient, ObjectId } from 'mongodb'
 import { Task } from 'packages/model/lib'
 import { RouterDefinition } from 'rest-ts-express'
 
 export const TaskRouter: RouterDefinition<typeof TaskApi> = {
   createTask: async req => {
     const newTask = req.body
+    // TODO: implement getting user id from session instead of 'abc123...'
     const task: Task = {
       ...newTask,
-      userId: 'TODO'
+      userId: 'abc123abc123abc123abc123'
     }
-    // TODO: check if parent exists, if is stated (undefined or existing)
-    // TODO: check if user exists
-    return task
+    const db = await MongoClient.connect('mongodb://localhost:27017/')
+    const dbo = db.db('open-gtd')
+    if (newTask.parentId !== undefined) {
+      if (
+        (await dbo
+          .collection('tasks')
+          .find({ _id: new ObjectId(task.parentId) })
+          .count()) === 0
+      ) {
+        // TODO: implement proper error handling
+        return 'parentId is set but does not exist in database'
+      }
+    }
+
+    if (
+      (await dbo
+        .collection('users')
+        .find({ _id: new ObjectId(task.userId) })
+        .count()) === 0
+    ) {
+      // TODO: implement proper error handling
+      return 'userId does not exist in database'
+    }
+
+    const insertedElement = await dbo.collection('tasks').insertOne(task)
+    return insertedElement.ops[0]
   },
   deleteTask: async req => {
     // const id = req.params.id
