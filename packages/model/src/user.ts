080b6ae0a86498ab81fa6bed4d392e2f285c078b
Erik Hofer <erik-hofer@mail.de>
2018-12-14 17:39:28 +0100

    #16 add credentials to model

--- packages/model/src/user.ts
+++ packages/model/src/user.ts
@@ -1,11 +1,7 @@
-import { Record, Static, String } from 'runtypes'
+import { Static } from 'runtypes'
 import { Entity } from './entity'
+import { Credentials } from './auth'
 
-export const User = Entity.And(
-  Record({
-    email: String,
-    password: String
-  })
-)
+export const User = Entity.And(Credentials)
 
 export type User = Static<typeof User>
