d8f27834e247ce49391c6112e79251b93fda957a
Tim Bestmann <tbestmann@web.de>
2018-12-14 12:53:53 +0100

    #15 add password hashing to user creation in database

--- packages/server/src/router/user.ts
+++ packages/server/src/router/user.ts
@@ -1,12 +1,12 @@
 import { UserApi } from '@open-gtd/api'
 import { User } from '@open-gtd/model'
+import { hash } from 'bcrypt'
 import { MongoClient } from 'mongodb'
 import { RouterDefinition } from 'rest-ts-express'
 
 export const UserRouter: RouterDefinition<typeof UserApi> = {
   createUser: async req => {
     const user = req.body as User
-    // TODO: hash password before storing in database
 
     const db = await MongoClient.connect('mongodb://localhost:27017/')
     const dbo = await db.db('open-gtd')
@@ -19,7 +19,11 @@ export const UserRouter: RouterDefinition<typeof UserApi> = {
       // TODO: implement proper error handling
       return 'mail address is already in use'
     } else {
-      const insertedElement = await dbo.collection('users').insertOne(user)
+      const insertUser : User = {
+        ...user,
+        password: await hash(user.password, 10)
+  }
+      const insertedElement = await dbo.collection('users').insertOne(insertUser)
       return insertedElement.ops[0]
     }
   }
