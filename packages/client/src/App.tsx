9d0ac0c7204dde75229b06b727f749b67d0727b8
Toby M <toby.moeller@gmx.de>
2018-12-16 19:38:26 +0100

    #41 routing for registration and home

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,19 +1,29 @@
-import { Rate } from 'antd'
 import * as React from 'react'
+import { Route, Router, Switch } from 'react-router-dom'
+
+import { Registration } from './components/Registration'
+import { Home } from './components/Home'
+import { history } from './history'
+
 import './App.scss'
+import { Layout } from 'antd';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
+      <Router history={history}>
+        <div className="App">
+          <div>
+          <Layout>
+            <Switch>
+              <Route path="/registration" component={Registration} />
+              <Route path="/" exact component={Home} /> 
+            </Switch>
+          </Layout>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Rate character="6" />
-      </div>
+      </Router>
+      
     )
   }
 }
