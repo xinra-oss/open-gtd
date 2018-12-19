52a5db0d75899eece33c35dc94af89d064542f72
Erik Hofer <erik-hofer@mail.de>
2018-12-19 20:27:31 +0100

    remove void response body from createSession

--- packages/api/src/auth.ts
+++ packages/api/src/auth.ts
@@ -1,11 +1,9 @@
 import { defineAPI, DELETE, POST } from 'rest-ts-core'
-import { Void } from 'runtypes'
 import { Credentials } from './model'
 
 export const AuthApi = defineAPI({
   createSession: POST `/session` // prettier-ignore
-    .body(Credentials)
-    .response(Void),
+    .body(Credentials),
   deleteSession: DELETE `/session` // prettier-ignore
   // TODO: getCsrfToken
 })
