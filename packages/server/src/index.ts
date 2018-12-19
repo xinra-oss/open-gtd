3a3921a92522b93d6fbbee447d5700487b1101fb
Erik Hofer <erik-hofer@mail.de>
2018-12-19 17:18:40 +0100

    #48 use winston for logging

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,17 +1,14 @@
 import * as BodyParser from 'body-parser'
-import debug from 'debug'
 import * as express from 'express'
 import * as session from 'express-session'
 import { authMiddleware, sessionConfiguration } from './auth'
 import { config } from './config'
 import { db } from './db'
+import { logger } from './logging'
 import { OpenGtdRouter } from './router'
 
-debug.enable('*')
-const log = debug('server')
-
 async function main() {
-  log('Starting server...')
+  logger.info('Starting server...')
   await db.connect()
 
   const app = express()
@@ -21,6 +18,6 @@ async function main() {
   app.use('/api', OpenGtdRouter)
 
   const port = config.get('port')
-  app.listen(port, () => log('Server is listening on port %s', port))
+  app.listen(port, () => logger.info('Server is listening on port %s', port))
 }
 main()
