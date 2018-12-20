c2ecda4208fcfae1c29eb6251139acd38f97d0dc
Erik Hofer <erik-hofer@mail.de>
2018-12-20 23:59:53 +0100

    #17 work on error handling

--- packages/client/src/store/epic/user.epic.ts
+++ packages/client/src/store/epic/user.epic.ts
@@ -1,6 +1,6 @@
-import { HttpException } from '@open-gtd/api'
 import { combineEpics } from 'redux-observable'
-import { filter, map, switchMap } from 'rxjs/operators'
+import { from, of } from 'rxjs'
+import { catchError, filter, map, switchMap } from 'rxjs/operators'
 import { isActionOf } from 'typesafe-actions'
 import { AppEpic } from '.'
 import { userActions } from '../actions'
@@ -8,15 +8,12 @@ import { userActions } from '../actions'
 const createUser: AppEpic = (action$, state$, { openGtdApi }) =>
   action$.pipe(
     filter(isActionOf(userActions.createUser.request)),
-    switchMap(action => openGtdApi.createUser({ body: action.payload })),
-    map(res => {
-      if (res.status === 200) {
-        return userActions.createUser.success(res.data)
-      }
-      return userActions.createUser.failure(
-        (res.data as unknown) as HttpException
+    switchMap(action =>
+      from(openGtdApi.createUser({ body: action.payload })).pipe(
+        map(res => userActions.createUser.success(res.data)),
+        catchError(err => of(userActions.createUser.failure(err)))
       )
-    })
+    )
   )
 
 export const userEpic = combineEpics(createUser)
