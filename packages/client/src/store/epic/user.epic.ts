4db360f0e42ae6b997b8ac1037992716a1105fce
Erik Hofer <erik-hofer@mail.de>
2018-12-21 00:10:29 +0100

    #17 refactor error handling in epic

--- packages/client/src/store/epic/user.epic.ts
+++ packages/client/src/store/epic/user.epic.ts
@@ -1,5 +1,5 @@
 import { combineEpics } from 'redux-observable'
-import { from, of } from 'rxjs'
+import { of } from 'rxjs'
 import { catchError, filter, map, switchMap } from 'rxjs/operators'
 import { isActionOf } from 'typesafe-actions'
 import { AppEpic } from '.'
@@ -8,12 +8,9 @@ import { userActions } from '../actions'
 const createUser: AppEpic = (action$, state$, { openGtdApi }) =>
   action$.pipe(
     filter(isActionOf(userActions.createUser.request)),
-    switchMap(action =>
-      from(openGtdApi.createUser({ body: action.payload })).pipe(
-        map(res => userActions.createUser.success(res.data)),
-        catchError(err => of(userActions.createUser.failure(err)))
-      )
-    )
+    switchMap(action => openGtdApi.createUser({ body: action.payload })),
+    map(res => userActions.createUser.success(res.data)),
+    catchError(err => of(userActions.createUser.failure(err)))
   )
 
 export const userEpic = combineEpics(createUser)
