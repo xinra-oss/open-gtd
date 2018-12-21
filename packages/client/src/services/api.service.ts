0190b74b31f389c94302b2423ea13b0d61c778d5
Erik Hofer <erik-hofer@mail.de>
2018-12-21 17:07:11 +0100

    #17 implememt reconstruction of exception type

--- packages/client/src/services/api.service.ts
+++ packages/client/src/services/api.service.ts
@@ -1,4 +1,10 @@
-import { OpenGtdApi } from '@open-gtd/api'
+import {
+  HttpException,
+  OpenGtdApi,
+  TypedHttpException,
+  UnauthorizedHttpException,
+  ValidationException
+} from '@open-gtd/api'
 import Axios from 'axios'
 import { createConsumer } from 'rest-ts-axios'
 import { of } from 'rxjs'
@@ -15,4 +21,42 @@ export type OpenGtdApiConsumer = typeof openGtdApi
 export const handleOpenGtdApiError: ApiErrorHandler = failureActionCreator => (
   err,
   caught
-) => of(failureActionCreator(err))
+) => {
+  // See https://gist.github.com/fgilio/230ccd514e9381fafa51608fcf137253
+  if (err.response) {
+    // The request was made and the server responded with a non 2xx status code
+    err = reconstructOriginalHttpException(err.response.data)
+  }
+  return of(failureActionCreator(err))
+}
+
+interface ClassType<T> {
+  new (...args: any[]): T
+}
+
+/**
+ * Exception types that should recustructed. Every other `TypedException` is
+ * turned into an `HttpException`.
+ */
+const RECONSTRUCTABLE_HTTP_EXCEPTIONS: ReadonlyArray<
+  ClassType<HttpException>
+> = [ValidationException, UnauthorizedHttpException]
+
+/**
+ * Creates an instance of the original exception type that can be used with
+ * `instanceof`.
+ */
+function reconstructOriginalHttpException(
+  typedException: TypedHttpException
+): HttpException {
+  let reconstructedType: ClassType<HttpException> = HttpException
+  for (const reconstructableType of RECONSTRUCTABLE_HTTP_EXCEPTIONS) {
+    if (reconstructableType.name === typedException.type) {
+      reconstructedType = reconstructableType
+    }
+  }
+  const reconstructedException = { ...typedException }
+  delete reconstructedException.type
+  Object.setPrototypeOf(reconstructedException, reconstructedType.prototype)
+  return (reconstructedException as unknown) as HttpException
+}
