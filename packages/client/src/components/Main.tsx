276b8ef7f86e98e0b82cfe13c65207ac479a5a1f
Toby M <toby.moeller@gmx.de>
2018-12-27 21:06:16 +0100

    #57 updated slider

--- packages/client/src/components/Main.tsx
+++ packages/client/src/components/Main.tsx
@@ -43,31 +43,20 @@ export class Main extends React.Component {
           defaultOpenKeys={['sub1']}
           style={{ height: '100%', borderRight: 0 }}
         >
-          <SubMenu key="inbox" title={<span><Icon type="user" />subnav</span>}>
-            <Menu.Item key="1">option1</Menu.Item>
-            <Menu.Item key="2">option2</Menu.Item>
-            <Menu.Item key="3">option3</Menu.Item>
-            <Menu.Item key="4">option4</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
-            <Menu.Item key="5">option5</Menu.Item>
-            <Menu.Item key="6">option6</Menu.Item>
-            <Menu.Item key="7">option7</Menu.Item>
-            <Menu.Item key="8">option8</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
-            <Menu.Item key="9">option9</Menu.Item>
-            <Menu.Item key="10">option10</Menu.Item>
-            <Menu.Item key="11">option11</Menu.Item>
-            <Menu.Item key="12">option12</Menu.Item>
+          <Menu.Item key="inbox">
+            <Icon type="inbox" style={{ fontSize: '16px'}} />
+            inbox
+          </Menu.Item>
+          <SubMenu key="projekt" title={<span><Icon type="folder" />projekt</span>}>
+            <Menu.Item key="1">Task 1</Menu.Item>
+            <Menu.Item key="2">Task 2</Menu.Item>
           </SubMenu>
         </Menu>
       </Sider>
       <Layout style={{ padding: '0 24px 24px' }}>
         <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
+          <Breadcrumb.Item>Projekt</Breadcrumb.Item>
+          <Breadcrumb.Item>Task1</Breadcrumb.Item>
         </Breadcrumb>
         <Content style={{
           background: '#fff', padding: 24, margin: 0, minHeight: 280,
