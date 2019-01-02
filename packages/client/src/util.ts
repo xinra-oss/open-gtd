772a98754c61c9d093bf1cd1c61a44daaf9340e6
Erik Hofer <erik-hofer@mail.de>
2019-01-02 18:07:46 +0100

    #83 implement display of validation errors

--- packages/client/src/util.ts
+++ packages/client/src/util.ts
@@ -1,3 +1,4 @@
+import { ValidationErrors } from '@open-gtd/api'
 import { Dictionary } from 'ts-essentials'
 
 export function arrayToDictionary<T>(
@@ -8,4 +9,18 @@ export function arrayToDictionary<T>(
     dict[mapItemToKey(item)] = item
     return item
   }, {})
+}
+
+export function getFormItemValidationProps<T>(
+  validationErrors: ValidationErrors<T>,
+  propertyName: keyof T
+): { validateStatus?: 'error'; help?: string } {
+  const errors = validationErrors[propertyName]
+  if (errors !== undefined) {
+    return {
+      validateStatus: 'error',
+      help: Array.isArray(errors) ? errors.join('\n') : errors.toString()
+    }
+  }
+  return {}
 }
