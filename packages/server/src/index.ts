3d97f0f91857e2706081a564eee7a0fd3ed84a46
Erik Hofer <erik-hofer@mail.de>
2019-01-04 13:45:49 +0100

    #80 add PUT to CORS header

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -23,7 +23,7 @@ async function main() {
   app.use(
     cors({
       origin: ['http://localhost:3000'],
-      methods: ['GET', 'POST', 'DELETE'],
+      methods: ['GET', 'POST', 'DELETE', 'PUT'],
       credentials: true
     })
   )
