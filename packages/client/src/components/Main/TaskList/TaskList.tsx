e1d22824bfe905101121578ca57ce6ad807f51d1
Toby M <toby.moeller@gmx.de>
2019-01-05 15:33:44 +0100

    creat root task, sub task and normal task

--- packages/client/src/components/Main/TaskList/TaskList.tsx
+++ packages/client/src/components/Main/TaskList/TaskList.tsx
@@ -110,6 +110,21 @@ class TaskList extends React.Component<TaskListProps, TaskListState> {
   }
 
   private handleNewTask = () => {
+    if (this.state.selectedTaskIds.length > 0) {
+      this.createNewTask(
+        this.props.tasks[this.state.selectedTaskIds[0]]._id,
+        'New Tast'
+      )
+    } else {
+      this.createNewTask(null, 'New Root Task')
+    }
+  }
+
+  private handleSubTask = () => {
+    this.createNewTask(this.state.selectedTaskIds[0], 'New Sub Task')
+  }
+
+  private createNewTask = (parentID: string | null, testTitle: string) => {
     const newTask: Task = {
       contextIds: [],
       dueDate: null,
@@ -118,9 +133,9 @@ class TaskList extends React.Component<TaskListProps, TaskListState> {
       isNeverActive: true,
       isProject: false,
       notes: null,
-      parentId: null,
+      parentId: parentID,
       startDate: null,
-      title: '<New Task>'
+      title: testTitle
     }
     this.props.dispatch(taskActions.createTask.request(newTask))
   }
@@ -142,6 +157,7 @@ class TaskList extends React.Component<TaskListProps, TaskListState> {
           <Button
             type="primary"
             icon="plus-square"
+            onClick={this.handleSubTask}
             disabled={selectedTaskIds.length !== 1}
           >
             New subtask
